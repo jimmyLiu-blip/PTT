3e88a45152dd4af2fbce8ef314b51d7f0b434350
星驛 <a0978262702@gmail.com>
2025-10-28 23:07:05 +0800

    第一次上傳

---
+++ ExcelToWord_Practice/ExcelToWord_Practice/ExcelToWord.Service/IExcelService.cs
@@ -0,0 +1,13 @@
+﻿using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelToWord.Service
+{
+    public interface IExcelService
+    {
+        Excel.Workbook Workbook { get; }
+
+        Excel.Range GetNameRange(Excel.Worksheet ws, string rangeName);
+
+        void Close();
+    }
+}
---
+++ ExcelToWord_Practice/ExcelToWord_Practice/ExcelToWord_Configurement/ExportSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelToWord.Configuration
+{
+    public class ExportSettings
+    {
+        public string ExcelPath { get; set; } = @"C:\Reports\5GNR_3.7GHz_4.5GHz.xlsx";
+
+        public string OutputFolder { get; set; } = @"C:\Reports\WordOutputs_ByItem";
+
+        public string[] TargetNames { get; set; } = { "ACL_1","ACLN_1" };
+
+        public int StartSheetIndex { get; set; } = 7;
+
+        public float WidthCm { get; set; } = 18;
+
+        public int DelayMs { get; set; } = 200;
+    }
+}
